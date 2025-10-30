21750121f838a6b63d988f70c8c2df5eba74f424
Yuge Zhang <scottyugochang@gmail.com>
2025-10-30 12:36:06 +0800

    .

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,4 +1,5 @@
 import '@mantine/core/styles.css';
+import 'mantine-datatable/styles.css';
 
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import { initialize, mswLoader } from 'msw-storybook-addon';
